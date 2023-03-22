c223f9342d97d69db4a57a53b7e3fa91e3a0fd9a
Devmatrix <devmatrix73@gmail.com>
2023-03-22 01:31:13 +0100

    #2, Add reserveApp function to validate form inputs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,7 +1,7 @@
 /***********************************************************
  *                     TOP NAVIGATION                     *
  ***********************************************************/
-function editNav() {
+ function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -17,11 +17,22 @@ function editNav() {
 /**
  * DOM Elements
  */
+// Modal
 const modalbg       = document.querySelector(".bground");
 const modalBtn      = document.querySelectorAll(".modal-btn");
-const formData      = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelector(".close");
 const modalContent  = document.querySelector(".content");
+// Form
+const form          = document.querySelector("form");
+const formData      = document.querySelectorAll(".formData");
+
+/**
+ * Set up variable to hold Regular expression object
+ */ 
+const regExpFullName   = new RegExp("^[a-zA-Z]([a-zA-Z\-\s]){1,30}$", "g");
+const regExpEmail      = new RegExp("^[a-zA-Z0-9\.\-\_]{1,30}@[a-zA-Z\-\_]{2,30}\.[a-zA-Z\-\_]{2,15}$", "g");
+const regexBirthdate   = new RegExp("^[0-9]{4}(\-[0-9]{2}){2}$", "g");
+const regexQuantity    = new RegExp("^[0-9]{1,2}$", "g");
 
 /* ----------------------------------
      LAUNCH AND CLOSE MODAL EVENT
@@ -64,3 +75,172 @@ const closeModal = () => {
 }
 closeModalBtn.addEventListener("click", closeModal);
 
+/* ----------------------------------
+          HANDLE RESERVE FORM                        
+   ---------------------------------- */
+
+const reserveApp = function() {
+  /**
+   * Focus and blur field
+   */
+  formData.forEach((child) => {
+    const input = child.querySelector("input");
+    // console.log(input);
+    const isInputFocus = (event) => {
+      event.target.style.background = "#e8f0fe";
+    }
+    
+    const isInputBlur = (event) => {
+      event.target.style.background = "";
+    }
+    
+    input.addEventListener("focus", isInputFocus, false);
+    input.addEventListener("blur", isInputBlur, false);
+  })
+
+  /**
+   * Test firstname value
+   */
+  function testFirstName() {
+    if (form.first.value.match(regExpFullName)) {
+      console.log(form.first.value);
+      form.first.style.borderColor = "transparent";
+      return true;
+    } else {
+      form.first.style.borderWidth = "3px";
+      form.first.style.borderColor = "rgb(254 20 85)";
+      console.log("Merci de saisir au moins 2 caractères pour le champ prénom.");
+      return false;
+    }
+  }
+  
+  /**
+   * Test name value
+   */
+  function testLastName() {
+    if (form.last.value.match(regExpFullName)) {
+      console.log(form.last.value);
+      form.last.style.borderColor = "transparent";
+      return true;
+    } else {
+      form.last.style.borderWidth = "3px";
+      form.last.style.borderColor = "rgb(254 20 85)";
+      console.log("Merci de saisir au moins 2 caractères pour le champ nom.");
+      return false;
+    }
+  }
+  
+  /**
+   * Test email value
+   */
+  function testEmail() {
+    if (regExpEmail.test(form.email.value)) {
+      console.log(form.email.value);
+      form.email.style.borderColor = "transparent";
+      return true;
+    } else {
+      form.email.style.borderWidth = "3px";
+      form.email.style.borderColor = "rgb(254 20 85)";
+      console.log("Merci de saisir une adresse valide.");
+      return false;
+    }
+  }
+  
+  /**
+   * Test birthdate value
+   */
+   function testBirthdate() {
+    if (regexBirthdate.test(form.birthdate.value)) {
+      console.log(form.birthdate.value);
+      form.birthdate.style.borderColor = "transparent";
+      return true;
+    } else {
+      form.birthdate.style.borderWidth = "3px";
+      form.birthdate.style.borderColor = "rgb(254 20 85)";
+      console.log("Merci de saisir votre date de naissance.");
+      return false;
+    }
+  }
+  
+  /**
+   * Test quantity value
+   */
+   function testQuantity() {
+    if (regexQuantity.test(form.quantity.value)) {
+      console.log(form.quantity.value);
+      form.quantity.style.borderColor = "transparent";
+      return true;
+    } else {
+      form.quantity.style.borderWidth = "3px";
+      form.quantity.style.borderColor = "rgb(254 20 85)";
+      console.log("Merci d'indiquer le nombre de tournoi.");
+      return false;
+    }
+  }
+  
+  /**
+   * Test location value
+   */
+  function testLocation() {
+    if ( form.location1.checked
+      || form.location2.checked
+      || form.location3.checked
+      || form.location4.checked
+      || form.location5.checked
+      || form.location6.checked) {
+      console.log(form.location.value);
+      return true;
+    } else {
+      console.log("Merci d'indiquer le tournoi.");
+      return false;
+    }
+  }
+  
+  /**
+   * Test if a box is checked
+   */
+  function testTermsChecked() {
+    if (form.checkbox1.checked) {
+      console.log("Conditions d'utilisation: " + form.checkbox1.checked );
+      return true;
+    } else {
+      console.log("Merci de lire et d'accepter les conditions d'utilisation.");
+      return false;
+    }
+  }
+  function testNewsChecked() { // optional checkbox
+    if (form.checkbox2.checked) {
+      console.log("checkbox2: " + form.checkbox2.checked);
+      return true;
+    }
+  }
+
+  /**
+   * Valid inputs
+   */
+  function isValid() {
+    testFirstName();
+    testLastName();
+    testEmail();
+    testBirthdate();
+    testQuantity();
+    testLocation();
+    testTermsChecked();
+    testNewsChecked();
+  }
+
+  /**
+   * Handle register form submission
+   * when submit event is fires
+   */
+  const validate = (e) => {    
+    if ((testFirstName() === true) && (testLastName() === true) && (testEmail() === true) && (testBirthdate() === true) && (testQuantity() === true) && (testLocation() === true) && (testTermsChecked() === true)) {
+      console.log("Merci pour votre inscription.");
+    } else {
+      e.preventDefault(); // Prevent the form being submitted
+      isValid();
+    }
+  }
+  form.addEventListener("submit", validate, false);
+}
+reserveApp();
